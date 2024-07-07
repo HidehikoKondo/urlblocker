c1d77b3e4fcaef8abb578765242cbac9355a7e03
Hidehiko Kondo <dev@udonko.net>
2024-07-08 00:25:26 +0900

    リダイレクト時にエラーが出てたの修正 あと、いらないコード消した

--- html/popup.js
+++ html/popup.js
@@ -1,44 +1,55 @@
-
-// let rejectedURLs = JSON.parse(localStorage.getItem('rejectedURLs')) || [];
-
 //urlの保存用
 let rejectedURLs = [];
-
-function check() {
-    console.log("--ccheck--");
-    console.log(rejectedURLs);
-}
 
 //rejectedURLsを取得して、なければ空っぽで作る
 chrome.storage.local.get(['rejectedURLs'], function (result) {
     rejectedURLs = result.rejectedURLs || [];
 
-    check();
+    //データ表示を更新
+    displayURLs();
+
+});
+
+// ページが読み込まれたときに実行
+document.addEventListener('DOMContentLoaded', () => {
+    //データ表示を更新
     displayURLs();
 });
 
 
+function addButton() {
+    // +ボタンでブロックするURLを追加
+    document.getElementById('addbutton').onclick = () => {
+        const url = document.getElementById('rejectURL').value;
+        if (url) {
+            addURL(url);
+            // テキストボックスをクリア
+            document.getElementById('rejectURL').value = '';
+        }
+        //データを表示更新
+        displayURLs();
+    };
+}
 
+//データの表示を更新する
 function displayURLs() {
-    console.log("displayURLS");
     //id="blockedurls"の子要素をすべて削除
     const blockedURLsElement = document.getElementById('blockedurls');
+    console.log("blockedURLsElement");
+    console.log(blockedURLsElement);
+    if (blockedURLsElement === null) {
+        return;
+    }
     while (blockedURLsElement.firstChild) {
         blockedURLsElement.removeChild(blockedURLsElement.firstChild);
     }
 
-    //ローカルストレージからデータを読み込み
-    // chrome.storage.local.get(['rejectedURLs'], function (result) {
-    //     let rejectedURLs = result.rejectedURLs || [];
 
-
-    console.log(rejectedURLs.length);
     // テキストボックスと×ボタンを作成して、blockedURLsElementに追加する
     rejectedURLs.forEach(url => {
         const container = document.createElement('div');
         container.classList.add('url-container');
 
-        console.log(url);
         const input = document.createElement('input');
         input.type = 'text';
         input.value = url;
@@ -55,59 +66,19 @@ function displayURLs() {
 
         blockedURLsElement.appendChild(container);
     });
-
-
-    // });
-
-
-
-    // let rejectedURLs = JSON.parse(localStorage.getItem('rejectedURLs')) || [];
-
-
 }
 
-
-
-
-
-
-
-
-// chrome.storage.local.set({ "title": "タイトル" }, function () {
-//     console.log("title set");
-// });
-
-// ページが読み込まれたときに実行
-document.addEventListener('DOMContentLoaded', () => {
-    //データを表示
-    displayURLs();
-
-    // +ボタンでブロックするURLを追加
-    document.getElementById('addbutton').onclick = () => {
-        const url = document.getElementById('rejectURL').value;
-        if (url) {
-            addURL(url);
-            document.getElementById('rejectURL').value = ''; // テキストボックスをクリア
-        }
-        //データを表示更新
-        console.log("tapped");
-        displayURLs();
-    };
-});
-
-
-// URLをrejectedURLsに追加し、localStorageに保存する関数
+// URLをrejectedURLsに追加し、localStorageに保存する
 function addURL(url) {
     if (!rejectedURLs.includes(url)) {
         rejectedURLs.push(url);
-        //localStorage.setItem('rejectedURLs', JSON.stringify(rejectedURLs));
-        // displayURLs(); // URLを表示する関数を呼び出す
 
         chrome.storage.local.set({ "rejectedURLs": rejectedURLs }, function () {
             console.log("rejectedURLs set");
         });
     }
-    displayURLs(); // 更新されたURLを表示する
+    //表示を更新
+    displayURLs();
 }
 
 // URLをrejectedURLsから削除し、localStorageに保存する関数
@@ -116,6 +87,8 @@ function removeURL(url) {
     chrome.storage.local.set({ "rejectedURLs": rejectedURLs }, function () {
         console.log("rejectedURLs set");
     });
-    displayURLs(); // 更新されたURLを表示する
+
+    //表示を更新
+    displayURLs();
 }
 
--- urlblocker.js
+++ urlblocker.js
@@ -1,41 +1,13 @@
 // 現在のページのURLを取得
 let currentURL = window.location.href;
-console.log(currentURL);
 
-// 拒否リストのurl
+
 chrome.storage.local.get("rejectedURLs", function (rejectedURLs) {
-    // console.info(data)
-
-    // let rejectedURLs = [];
-
-    console.log("--rejectedURLs--");
-    console.log(rejectedURLs);
-
-
-    // currentURLがrejectedURLsに含まれているかチェック
-    //    const isURLRejected = rejectedURLs.includes(currentURL);
-
-    //    console.log(isURLRejected); // trueまたはfalseが出力される
-
-
     // 現在のURLがrejectedURLsに含まれるURLの文字列を含んでいるか確認
     let isBlocked = rejectedURLs["rejectedURLs"].some(rejectedURL => currentURL.indexOf(rejectedURL) !== -1);
 
-    // let isBlocked = false;
-
-    // rejectedURLs["rejectedURLs"].forEach(url => {
-    //     console.log(url);
-    // });
-
+    // blocked.htmlにリダイレクト
     if (isBlocked) {
-        // blocked.htmlにリダイレクト
-        console.log("blocked");
         window.location.href = chrome.runtime.getURL('./html/blocked.html');
-    } else {
-        console.log("not blocked");
     }
 });
-
-// rejectedURLsをlocalStorageから読み込む。存在しない場合は空の配列を使用
-//let rejectedURLs = JSON.parse(localStorage.getItem('rejectedURLs')) || [];
-
