ef35e2d946fb17c67a717d2313f8d17f995ef20a
Hidehiko Kondo <dev@udonko.net>
2024-07-07 23:59:21 +0900

    データのやり取りをlocalStrageからchrome.storage.localに変更

---
+++ background.js
@@ -0,0 +1,33 @@
+
+// this.intervalId = setInterval(() => {
+//     chrome.storage.local.get("rejectedURLs", function (data) {
+//         console.info(data)
+
+
+
+//     });
+
+//     // let rejectedURLs = [];
+//     // //rejectedURLsを取得して、なければ空っぽで作る
+//     // chrome.storage.local.get(['rejectedURLs'], function (result) {
+//     //     rejectedURLs = result.rejectedURLs || [];
+//     // });
+
+//     // console.log("--rejectedURLs--");
+//     // console.log(rejectedURLs);
+
+
+// }, 5000);
+
+// // chrome.storage.local.get("title", function (data) { console.info(data) });
+// // chrome.storage.local.get("title", function (data) { console.info(data) });
+// // chrome.storage.local.get("title", function (data) { console.info(data) });
+
+
+// //データ取り出し
+// // chrome.strage.local.get("data", function (data) { console.info(data) });
+
+// //データ格納
+// // chrome.storage.local.set({ "data": ["aaa", "bbb"] }, function () { console.log("data set AB") });
+
+
--- html/popup.js
+++ html/popup.js
@@ -1,42 +1,25 @@
-// rejectedURLsをlocalStorageから読み込む。存在しない場合は空の配列にする
-let rejectedURLs = JSON.parse(localStorage.getItem('rejectedURLs')) || [];
 
-// ページが読み込まれたときに実行
-document.addEventListener('DOMContentLoaded', () => {
-    //データを表示
+// let rejectedURLs = JSON.parse(localStorage.getItem('rejectedURLs')) || [];
+
+//urlの保存用
+let rejectedURLs = [];
+
+function check() {
+    console.log("--ccheck--");
+    console.log(rejectedURLs);
+}
+
+//rejectedURLsを取得して、なければ空っぽで作る
+chrome.storage.local.get(['rejectedURLs'], function (result) {
+    rejectedURLs = result.rejectedURLs || [];
+
+    check();
     displayURLs();
-
-    // +ボタンでブロックするURLを追加
-    document.getElementById('addbutton').onclick = () => {
-        const url = document.getElementById('rejectURL').value;
-        if (url) {
-            addURL(url);
-            document.getElementById('rejectURL').value = ''; // テキストボックスをクリア
-        }
-        //データを表示更新
-        displayURLs();
-    };
 });
 
 
-// URLをrejectedURLsに追加し、localStorageに保存する関数
-function addURL(url) {
-    if (!rejectedURLs.includes(url)) {
-        rejectedURLs.push(url);
-        localStorage.setItem('rejectedURLs', JSON.stringify(rejectedURLs));
-        // displayURLs(); // URLを表示する関数を呼び出す
-    }
-}
-
-// URLをrejectedURLsから削除し、localStorageに保存する関数
-function removeURL(url) {
-    rejectedURLs = rejectedURLs.filter(u => u !== url);
-    localStorage.setItem('rejectedURLs', JSON.stringify(rejectedURLs));
-    displayURLs(); // 更新されたURLを表示する
-}
 
 function displayURLs() {
-
     console.log("displayURLS");
     //id="blockedurls"の子要素をすべて削除
     const blockedURLsElement = document.getElementById('blockedurls');
@@ -45,12 +28,17 @@ function displayURLs() {
     }
 
     //ローカルストレージからデータを読み込み
-    let rejectedURLs = JSON.parse(localStorage.getItem('rejectedURLs')) || [];
+    // chrome.storage.local.get(['rejectedURLs'], function (result) {
+    //     let rejectedURLs = result.rejectedURLs || [];
+
+
+    console.log(rejectedURLs.length);
     // テキストボックスと×ボタンを作成して、blockedURLsElementに追加する
     rejectedURLs.forEach(url => {
         const container = document.createElement('div');
         container.classList.add('url-container');
 
+        console.log(url);
         const input = document.createElement('input');
         input.type = 'text';
         input.value = url;
@@ -69,4 +57,65 @@ function displayURLs() {
     });
 
 
+    // });
+
+
+
+    // let rejectedURLs = JSON.parse(localStorage.getItem('rejectedURLs')) || [];
+
+
 }
+
+
+
+
+
+
+
+
+// chrome.storage.local.set({ "title": "タイトル" }, function () {
+//     console.log("title set");
+// });
+
+// ページが読み込まれたときに実行
+document.addEventListener('DOMContentLoaded', () => {
+    //データを表示
+    displayURLs();
+
+    // +ボタンでブロックするURLを追加
+    document.getElementById('addbutton').onclick = () => {
+        const url = document.getElementById('rejectURL').value;
+        if (url) {
+            addURL(url);
+            document.getElementById('rejectURL').value = ''; // テキストボックスをクリア
+        }
+        //データを表示更新
+        console.log("tapped");
+        displayURLs();
+    };
+});
+
+
+// URLをrejectedURLsに追加し、localStorageに保存する関数
+function addURL(url) {
+    if (!rejectedURLs.includes(url)) {
+        rejectedURLs.push(url);
+        //localStorage.setItem('rejectedURLs', JSON.stringify(rejectedURLs));
+        // displayURLs(); // URLを表示する関数を呼び出す
+
+        chrome.storage.local.set({ "rejectedURLs": rejectedURLs }, function () {
+            console.log("rejectedURLs set");
+        });
+    }
+    displayURLs(); // 更新されたURLを表示する
+}
+
+// URLをrejectedURLsから削除し、localStorageに保存する関数
+function removeURL(url) {
+    rejectedURLs = rejectedURLs.filter(u => u !== url);
+    chrome.storage.local.set({ "rejectedURLs": rejectedURLs }, function () {
+        console.log("rejectedURLs set");
+    });
+    displayURLs(); // 更新されたURLを表示する
+}
+
